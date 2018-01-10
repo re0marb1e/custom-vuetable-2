50d3bf551313d2c25532c8640ee3042b83527b3b
re0marb1e <zhxt1016@gmail.com>
2018-01-10 16:34:11 +0800

    add sorting support for table

--- src/components/MyVuetable.vue
+++ src/components/MyVuetable.vue
@@ -1,43 +1,92 @@
 <template>
-  <vuetable ref="vuetable"
-            api-url="http://vuetable.ratiw.net/api/users"
-            :fields="fields"
-            :css="css"
-  ></vuetable>
+  <div class="container">
+    <vuetable ref="vuetable"
+              api-url="http://vuetable.ratiw.net/api/users"
+              :fields="fields"
+              :css="css.table"
+              :per-page="20"
+              :multi-sort="true"
+              :sort-order="sortOrder"
+              pagination-path=""
+              @vuetable:pagination-data="onPaginationData"
+    ></vuetable>
+    <div class="vuetable-pagination">
+      <vuetable-pagination-info ref="paginationInfo"
+                                info-template="第 {from} 条到第 {to} 条, 总共 {total} 条"
+                                no-data-template="没有相关数据"
+                                class="pull-left">
+      </vuetable-pagination-info>
+      <vuetable-pagination ref="pagination"
+                           :css="css.pagination"
+                           @vuetable-pagination:change-page="onChangePage">
+      </vuetable-pagination>
+    </div>
+  </div>
 </template>
-
 <script>
   import Vuetable from 'vuetable-2/src/components/Vuetable'
+  import VuetablePagination from 'vuetable-2/src/components/VuetablePagination'
+  import VuetablePaginationInfo from 'vuetable-2/src/components/VuetablePaginationInfo'
 
   export default {
     components: {
-      Vuetable
+      Vuetable,
+      VuetablePagination,
+      VuetablePaginationInfo
     },
     data () {
       return {
         css: {
-          tableClass: 'table table-striped table-bordered'
+          table: {
+            tableClass: 'table table-striped table-bordered table-hovered',
+            loadingClass: 'loading',
+            ascendingIcon: 'glyphicon glyphicon-chevron-up',
+            descendingIcon: 'glyphicon glyphicon-chevron-down',
+            handleIcon: 'glyphicon glyphicon-menu-hamburger'
+          },
+          pagination: {
+            infoClass: 'pull-left',
+            wrapperClass: 'vuetable-pagination pull-right',
+            activeClass: 'btn-primary',
+            disabledClass: 'disabled',
+            pageClass: 'btn btn-border',
+            linkClass: 'btn btn-border',
+            icons: {
+              first: '',
+              prev: '',
+              next: '',
+              last: ''
+            }
+          }
         },
-        fields: ['name', 'email', 'birthdate',
+        fields: [
           {
+            name: 'name',
+            sortField: 'name'
+          }, {
+            name: 'email',
+            sortField: 'email'
+          }, {
+            name: 'birthdate'
+          }, {
+            name: 'age',
+            sortField: 'birthdate'
+          }, {
             name: 'nickname',
             callback: 'allcap'
           }, {
             name: 'gender',
             callback: 'genderLabel'
           }, {
-            name: 'salary'
-          }, {
-            name: 'address.line1',
-            title: 'Address 1',
-            titleClass: 'text-right',
-            dataClass: 'text-right'
-          }, {
-            name: 'address.line2',
-            title: 'Address 2'
-          }, {
-            name: 'address.zipcode',
-            title: 'Zipcode'
+            name: 'salary',
+            visible: false
+          }
+        ],
+        sortOrder: [  // default sort order
+          {
+            field: 'email',
+            sortField: 'email',
+            direction: 'desc'
           }
         ]
       }
@@ -50,7 +99,26 @@
         return value === 'M'
           ? '<span class="label label-info"><i class="glyphicon glyphicon-star"></i> Male</span>'
           : '<span class="label label-success"><i class="glyphicon glyphicon-heart"></i> Female</span>'
+      },
+      onPaginationData (paginationData) {
+        this.$refs.pagination.setPaginationData(paginationData)
+        this.$refs.paginationInfo.setPaginationData(paginationData)
+      },
+      onChangePage (page) {
+        this.$refs.vuetable.changePage(page)
       }
     }
   }
 </script>
+<style>
+  .pagination {
+    margin-top: 0;
+  }
+  .btn.btn-border {
+    border: 1px solid;
+    margin-right: 2px;
+  }
+  .vuetable-pagination-info {
+    margin-top: 8px !important;
+  }
+</style>
